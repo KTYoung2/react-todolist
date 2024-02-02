23eaed6b38717d2efdbdfc735e12e7bb4ac592cf
kty <xodud9625@hanmail.net>
2024-02-02 21:42:01 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -72,7 +72,6 @@ function App() {
   return ( 
     <>
     <GlobalStyle />
-      <ToDoList />
     </>
   );
 
--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,14 +1,34 @@
-import { atom } from "recoil";
+import { atom , selector } from "recoil";
 
+
+export enum Categories {
+  "TO_DO",
+  "DOING",
+  "DONE",
+}
 
 export interface IToDo {
     text : string;
     id : number;
-    category : "TO_DO" | "DOING" | "DONE";
+    category: Categories;
   }
   
+export const categoryState = atom<Categories>({
+  key : "category",
+  default : Categories.TO_DO,
+});
 
 export const toDoState = atom<IToDo[]>({
     key : "toDo",
     default : [],
   });
+
+
+export const toDoSelector = selector({
+    key: "toDoSelector",
+    get : ({get}) => {
+      const toDos = get(toDoState);
+      const categoty = get(categoryState);
+      return toDos.filter((toDo) => toDo.category === categoty);
+    },
+});
